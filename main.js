642d5fa4cbd2ca317e2ac23eb1341e83af741d30
Calo Corp <marc.calo@codeimmersives.com>
2020-04-14 08:39:15 -0400

    completed

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 const Stack = function() {
+ 
+const newobj = {
+  items : [],
+  add: function (thing){this.items.push(thing)},
+  remove: function(thing){return this.items.pop(thing)},
+  peek: function(){return this.items[this.items.length-1]}
 }
 
+return newobj
+  }
+
+
 const Queue = function() {
-}
+  const newObj = {
+
+    items: [],
+    add: function (thing){this.items.push(thing) },
+    remove: function (thing){return this.items.shift(thing)},
+    peek: function(){return this.items[0]}
+  }
+
+  return newObj
+  }
+
+  
+
 
 module.exports = {
   Stack,
